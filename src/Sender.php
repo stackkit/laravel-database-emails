6224cccc1fa535739d62437950f64091a43829ce
Marick van Tuil <info@marickvantuil.nl>
2017-12-14 14:37:11 +0100

    sender refactor

--- src/Sender.php
+++ src/Sender.php
@@ -2,9 +2,9 @@
 
 namespace Buildcode\LaravelDatabaseEmails;
 
+use Illuminate\Mail\Mailer;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\Mail;
 
 class Sender
 {
@@ -25,23 +25,45 @@ class Sender
             Event::dispatch('before.send');
         }
 
-        Mail::send([], [], function (Message $message) use ($email) {
-            $message->to($email->getRecipient())
-                ->cc($email->hasCc() ? $email->getCc() : [])
-                ->bcc($email->hasBcc() ? $email->getBcc() : [])
-                ->subject($email->getSubject())
-                ->from(config('mail.from.address'), config('mail.from.name'))
-                ->setBody($email->getBody(), 'text/html');
-
-            foreach ((array)$email->getAttachments() as $attachment) {
-                if ($attachment['type'] == 'attachment') {
-                    $message->attach($attachment['attachment']['file'], $attachment['attachment']['options']);
-                } else if($attachment['type'] == 'rawAttachment') {
-                    $message->attachData($attachment['attachment']['data'], $attachment['attachment']['name'], $attachment['attachment']['options']);
-                }
-            }
+        $this->getMailerInstance()->send([], [], function (Message $message) use ($email) {
+            $this->buildMessage($message, $email);
         });
 
         $email->markAsSent();
+    }
+
+    /**
+     * Get the instance of the Laravel mailer.
+     *
+     * @return Mailer
+     */
+    private function getMailerInstance()
+    {
+        return app('mailer');
+    }
+
+    /**
+     * Build the e-mail message.
+     *
+     * @param  Message $message
+     * @param  Email   $email
+     */
+    private function buildMessage(Message $message, Email $email)
+    {
+        $message->to($email->getRecipient())
+            ->cc($email->hasCc() ? $email->getCc() : [])
+            ->bcc($email->hasBcc() ? $email->getBcc() : [])
+            ->subject($email->getSubject())
+            ->from(config('mail.from.address'), config('mail.from.name'))
+            ->setBody($email->getBody(), 'text/html');
+
+        $attachmentMap = [
+            'attachment'    => 'attach',
+            'rawAttachment' => 'attachData',
+        ];
+
+        foreach ((array)$email->getAttachments() as $attachment) {
+            call_user_func_array([$message, $attachmentMap[$attachment['type']]], $attachment['attachment']);
+        }
     }
 }
